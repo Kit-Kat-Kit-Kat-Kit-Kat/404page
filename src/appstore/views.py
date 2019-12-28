0bc5c2b9d5bb0cce8ac266358322a6fc840a7834
Kit-Kat-Kit-Kat-Kit-Kat <58452758+Kit-Kat-Kit-Kat-Kit-Kat@users.noreply.github.com>
2019-12-27 16:36:31 -0800

    Update views.py

--- src/appstore/views.py
+++ src/appstore/views.py
@@ -22,7 +22,7 @@ def homePage(request):
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
     
-def handler404(request, exception, template_name='404.html'):
-    response = render(request, template_name)
+def handler404(request, exception, '404.html'):
+    response = render(request, '404.html')
     response.status_code = 404
     return response
